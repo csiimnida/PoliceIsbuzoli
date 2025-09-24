8c464681e08a090d008a760fbde907e0c13ce9ab
박준서 <pppg12jintpt@gmail.com>
2025-09-24 09:18:33 +0900

    feat:statLoad

--- Assets/Code/PJS/StatDataLoader.cs
+++ Assets/Code/PJS/StatDataLoader.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 using System.IO;
 using UnityEngine.SceneManagement;
 using System;
+using System.Linq;
+using System.Text;
 public class StatDataLoader : MonoBehaviour
 {
     private const string fileName = "Stat";
     public Dictionary<string, StatData> stats = new Dictionary<string, StatData>();
-
+    StreamReader reader;
+    StreamWriter writer;
     private void Awake()
     {
         SceneManager.sceneLoaded += HandleLoad;
@@ -22,18 +25,57 @@ public class StatDataLoader : MonoBehaviour
 
     private void HandleSave(Scene scene)
     {
-        StreamReader reader = new StreamReader(Application.dataPath + "/" + fileName);
+        writer = new StreamWriter(Application.dataPath + "/" + fileName);
+        List<string> type = new List<string>();
+        foreach(KeyValuePair<string, StatData> kvp in stats)
+        {
+            List<string> list = new List<string>() 
+            { 
+                kvp.Key, 
+                kvp.Value.Name, 
+                kvp.Value.toInt? ((int)kvp.Value.value).ToString() : kvp.Value.value.ToString(), 
+                kvp.Value.toInt.ToString() 
+            };
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < list.Count; i++)
+            {
+                sb.Append(list[i]);
+                sb.Append((i == list.Count - 1) ? ',' :'\0');
+            }
+            type.Add(sb.ToString());
+        }
+
+        foreach(string s  in type)
+        {
+            writer.WriteLine(s);
+        }
+        writer.Close();
     }
 
     private void HandleLoad(Scene scene, LoadSceneMode sceneMode)
     {
-        
+
+        reader = new StreamReader(Application.dataPath + "/" + fileName);
+        reader.Close();
+    }
+
+    public bool AddStat(string name, StatData data)
+    {
+        if (stats.ContainsKey(name)) return false;
+
+        stats.Add(name, data);
+        return true;
+    }
+
+    public void SetAllStat(Dictionary<string, StatData> target)
+    {
+        stats = target;
     }
 }
 
 public struct StatData
 {
-    public string key;
+    public string Name;
     public float value;
     public bool toInt;
 }
