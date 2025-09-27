886119ac0b67141633d20ba8921805299f0e1046
leemseungwon <leemsh1236@gmail.com>
2025-09-27 17:31:42 +0900

    Created InvestmentUI

---
+++ Assets/Code/LSW/Code/UI/InvestmentUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Code.LSW.Code.UI
+{
+    public class InvestmentUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject reachTabUI;
+        [SerializeField] private GameObject crackdownTabUI;
+        [SerializeField] private GameObject educationTabUI;
+        [SerializeField] private GameObject cooperationTabUI;
+        
+        public void OnClick(InvestmentUIType type)
+        {
+            switch (type)
+            {
+                case InvestmentUIType.Reach:
+                    reachTabUI.SetActive(true);
+                    crackdownTabUI.SetActive(false);
+                    educationTabUI.SetActive(false);
+                    cooperationTabUI.SetActive(false);
+                    break;
+                case InvestmentUIType.Crackdown:
+                    reachTabUI.SetActive(false);
+                    crackdownTabUI.SetActive(true);
+                    educationTabUI.SetActive(false);
+                    cooperationTabUI.SetActive(false);
+                    break;
+                case InvestmentUIType.Education:
+                    reachTabUI.SetActive(false);
+                    crackdownTabUI.SetActive(false);
+                    educationTabUI.SetActive(true);
+                    cooperationTabUI.SetActive(false);
+                    break;
+                case InvestmentUIType.Cooperation:
+                    reachTabUI.SetActive(false);
+                    crackdownTabUI.SetActive(false);
+                    educationTabUI.SetActive(false);
+                    cooperationTabUI.SetActive(true);
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Code/LSW/Code/UI/InvestmentUIButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Code.LSW.Code.UI
+{
+    public enum InvestmentUIType
+    {
+        Reach, 
+        Crackdown, 
+        Education, 
+        Cooperation
+    }
+
+    public class InvestmentUIButton : MonoBehaviour
+    {
+        [SerializeField] private InvestmentUI investmentUI;
+        public InvestmentUIType type;
+
+        public void OnClick()
+            => investmentUI?.OnClick(type);
+    }
+}
