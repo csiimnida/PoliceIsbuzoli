ab229f393b6650e9a0e2121ae4df65606554731b
leemseungwon <leemsh1236@gmail.com>
2025-09-27 22:16:27 +0900

    Fix HoverTransparencyUI

---
+++ Assets/Code/LSW/Code/UI/TimeSettingsUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Code.LSW.Code.UI
+{
+    public class TimeSettingsUI : MonoBehaviour
+    {
+        public void OnSetTime(float value)
+        {
+            Time.timeScale = value;
+        }
+    }
+}
