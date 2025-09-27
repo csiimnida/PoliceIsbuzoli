fb6efbe33101ef389e6e03adf314c6157267f5ff
박준서 <pppg12jintpt@gmail.com>
2025-09-27 17:32:19 +0900

    feat : StatIntgeraction

--- Assets/Code/PJS/UpgradeNode.cs
+++ Assets/Code/PJS/UpgradeNode.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class UpgradeNode : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private UpgradeNode[] RequestNode;
+    [SerializeField] private UpgradeNode[] NextNode;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float coast;
+
+    private bool _activated = false;
+    public bool Activated { get { return _activated; } }
+
+    private struct StatValuePair
     {
-        
+        public string Key;
+        public object Value;
+    }
+    public void TryActiveNode()
+    {
+        bool requestNodeActive = true;
+        foreach (UpgradeNode node in RequestNode)
+        {
+            if (node.Activated == false)
+            {
+                requestNodeActive = false;
+                continue;
+            }
+        }
+
+        if (requestNodeActive)
+        {
+            _activated = true;
+        }
     }
 }
